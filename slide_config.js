0a3c1451ab0d4552e023a06b34d6051e1d3b93b7
Erik Bjäreholt <erik.bjareholt@gmail.com>
2015-05-08 02:25:16 +0200

    WIP, draft

--- slide_config.js
+++ slide_config.js
@@ -1,18 +1,18 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: 'Python',
+    subtitle: 'A short introduction',
+    eventInfo: {
+      title: 'Code@LTH Meetup',
+      date: '19th of May 2015'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: '',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Erik Bjäreholt',
+    company: '',
+    gplus: '',
+    twitter: '',
+    www: 'http://erik.bjareholt.com',
+    github: 'http://github.com/ErikBjare'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
